6be65d07088ae8df6e6338932e2da364f78e45c1
Fyu11 <silvia.montors@gmail.com>
2020-06-13 10:58:46 +0200

    ...

--- src/main/java/com/uniroma3/montorsmeds/TaskManager/controller/AuthenticationController.java
+++ src/main/java/com/uniroma3/montorsmeds/TaskManager/controller/AuthenticationController.java
@@ -1,5 +1,22 @@
 package com.uniroma3.montorsmeds.TaskManager.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.uniroma3.montorsmeds.TaskManager.controller.validation.CredentialsValidator;
+import com.uniroma3.montorsmeds.TaskManager.controller.validation.UserValidator;
+import com.uniroma3.montorsmeds.TaskManager.model.Credentials;
+import com.uniroma3.montorsmeds.TaskManager.service.CredentialsService;
+
+@Controller
 public class AuthenticationController {
 
+	@Autowired
+	CredentialsService credentialsService;
+	
+	@Autowired
+	UserValidator userValidator;
+	
+	@Autowired
+	CredentialsValidator credentialsValidator;
 }
