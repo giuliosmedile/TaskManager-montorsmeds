a6702725a80338debded539a7a0ec513dde84d28
Fyu11 <silvia.montors@gmail.com>
2020-06-14 14:42:23 +0200

    taskManagerApplication

--- src/main/java/com/uniroma3/montorsmeds/TaskManager/TaskManagerApplication.java
+++ src/main/java/com/uniroma3/montorsmeds/TaskManager/TaskManagerApplication.java
@@ -1,7 +1,9 @@
 package com.uniroma3.montorsmeds.TaskManager;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class TaskManagerApplication {
@@ -9,5 +11,8 @@ public class TaskManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
+	
+	@Autowired
+	private Environment environment;
 	
 }
