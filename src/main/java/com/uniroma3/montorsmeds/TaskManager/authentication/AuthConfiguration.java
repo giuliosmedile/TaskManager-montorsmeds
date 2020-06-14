95ec67481de1dc8bd3b45035858162518c1c54fb
Fyu11 <silvia.montors@gmail.com>
2020-06-14 17:06:54 +0200

    ignore

--- src/main/java/com/uniroma3/montorsmeds/TaskManager/authentication/AuthConfiguration.java
+++ src/main/java/com/uniroma3/montorsmeds/TaskManager/authentication/AuthConfiguration.java
@@ -2,7 +2,9 @@ package com.uniroma3.montorsmeds.TaskManager.authentication;
 
 import javax.sql.DataSource;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -10,9 +12,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import javax.sql.DataSource;
 
 import static com.uniroma3.montorsmeds.TaskManager.model.Credentials.ADMIN_ROLE;
 
+@Configuration
+@EnableWebSecurity
 public class AuthConfiguration extends WebSecurityConfigurerAdapter{
 	
 	
@@ -46,6 +51,4 @@ public class AuthConfiguration extends WebSecurityConfigurerAdapter{
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-	
 }
--- src/main/java/com/uniroma3/montorsmeds/TaskManager/service/CredentialsService.java
+++ src/main/java/com/uniroma3/montorsmeds/TaskManager/service/CredentialsService.java
@@ -4,16 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.uniroma3.montorsmeds.TaskManager.authentication.AuthConfiguration;
 import com.uniroma3.montorsmeds.TaskManager.model.Credentials;
 import com.uniroma3.montorsmeds.TaskManager.repository.CredentialsRepository;
 
@@ -54,10 +50,5 @@ public class CredentialsService {
 			result.add(c);
 		}
 		return result;
-	}
-
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 }
